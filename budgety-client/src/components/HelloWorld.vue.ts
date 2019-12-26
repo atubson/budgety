308b5e836beb6a79beefb31ad02ba5406e565f17
atubson <jakubatub@gmail.com>
2019-12-26 11:59:20 +0100

    Add Login Register Component (#5)
    Related to 03-Formularz Logowania/Rejestracji Component

--- budgety-client/src/components/HelloWorld.vue.ts
+++ budgety-client/src/components/HelloWorld.vue.ts
@@ -1,8 +1,15 @@
 import { Component, Vue } from 'vue-property-decorator'
+import { FormComponent } from './common/Form/Form.vue'
 import WithRender from './HelloWorld.html'
+import './HelloWorld.css'
 
 @WithRender
-@Component
+@Component({
+  components: {
+    FormComponent
+  }
+})
 export default class HelloWorld extends Vue {
   public msg: string = 'I am using TypeScript classes with Vue!';
+  public isRegisterForm: boolean = true;
 }
---
+++ budgety-client/src/components/common/Form/RegisterFormModel.ts
@@ -0,0 +1,6 @@
+export class RegisterFormModel {
+  email: string = '';
+  name: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+}
